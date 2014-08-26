723a5d852cbe251e612ec7a750990608850cc457
norman martinez <norman.martinez@hotmail.com>
2014-08-26 20:32:04 -0300

    Initial Commit

---
+++ CharruaSurfReport/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CharruaSurfReport
+//
+//  Created by norman martinez on 26/08/14.
+//  Copyright (c) 2014 NMDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NMAppDelegate class]));
+    }
+}
