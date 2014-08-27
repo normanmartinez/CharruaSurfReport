f04d95b6b776ec43032ee41e3d6970079e678339
norman martinez <norman.martinez@hotmail.com>
2014-08-27 20:26:15 -0300

    ok

--- CharruaSurfReport/NMPicosListaTableViewController.m
+++ CharruaSurfReport/NMPicosListaTableViewController.m
@@ -59,7 +59,6 @@
 }
 
 
-
 /*
 // Override to support conditional editing of the table view.
 - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
---
+++ CharruaSurfReport/NMVPicosAgregariewController.h
@@ -0,0 +1,13 @@
+//
+//  NMVPicosAgregariewController.h
+//  CharruaSurfReport
+//
+//  Created by norman martinez on 27/08/14.
+//  Copyright (c) 2014 NMDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMVPicosAgregariewController : UIViewController
+
+@end
