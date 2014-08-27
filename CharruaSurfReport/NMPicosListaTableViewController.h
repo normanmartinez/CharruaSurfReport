448bee16d67859e15bd91d03c617249d5d524588
norman martinez <norman.martinez@hotmail.com>
2014-08-27 17:25:15 -0300

    Creación PicosListaViewController

---
+++ CharruaSurfReport/NMPicosListaTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NMPicosListaTableViewController.h
+//  CharruaSurfReport
+//
+//  Created by norman martinez on 27/08/14.
+//  Copyright (c) 2014 NMDevelopment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NMPicosListaTableViewController : UITableViewController
+
+@end
